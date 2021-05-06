e0982451b41aedf710916c6f1a01f1d775b89c6c
maini <philip.mattsson@iths.se>
2021-05-06 11:36:19 +0200

    first commit

---
+++ blomquiz-frontend/src/views/Game.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="game">
+    <img alt="" src="../assets/ProductBox.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Game.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
